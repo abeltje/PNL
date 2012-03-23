0a426357fd84ec51255f83c06b1861d3c18ecc91
Henk van Oers <hvo.pm@xs4all.nl>
2012-03-23 13:24:17 +0100

    Begin gemaakt met Introductie en About

--- lib/PNL.pm
+++ lib/PNL.pm
@@ -7,4 +7,28 @@ get '/' => sub {
     template 'index';
 };
 
+get '/about/' => sub {
+    template 'about';
+};
+
+get '/news/' => sub {
+    template 'news';
+};
+
+get '/amsterdam/' => sub {
+    template 'amsterdam';
+};
+
+get '/groningen/' => sub {
+    template 'groningen';
+};
+
+get '/workshop/' => sub {
+    template 'workshop';
+};
+
+get '/sppn/' => sub {
+    template 'sppn';
+};
+
 true;
