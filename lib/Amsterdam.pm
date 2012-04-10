1531d4fda11757e2c15ffd2293a78ae36ecc802a
Henk van Oers <hvo.pm@xs4all.nl>
2012-04-10 12:06:15 +0200

    Fix correct template for Amsterdam

--- lib/Amsterdam.pm
+++ lib/Amsterdam.pm
@@ -3,12 +3,10 @@ use Dancer ':syntax';      # Controler
 use Template;              # View
 use Dancer::Plugin::DBIC;  # Model
 
-use I18N::LangTags::List;  # languages
-
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+    template 'amsterdam';
 };
 
 true;
