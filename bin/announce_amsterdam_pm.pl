213cfcf9e7f133b7d5779048554d8bf45dbd0d52
Abe Timmerman <abeltje@cpan.org>
2020-03-30 15:07:56 +0200

    Corona, no meetups.

--- bin/announce_amsterdam_pm.pl
+++ bin/announce_amsterdam_pm.pl
@@ -41,6 +41,12 @@ my %amsterdam_place = (
         place  => 'Amsterdam-Duivendrecht',
         url    => 'https://www.mintlab.nl',
     },
+    nowhere => {
+        name   => 'Nergens',
+        street => '',
+        place  => '',
+        url    => '',
+    },
 );
 
 if (not exists $amsterdam_place{ $opt{place} }) {
@@ -56,75 +62,90 @@ sub send_email {
     my ($place) = @_;
     my @args = (
         next_amsterdam_meeting(),
-        $place->{name},
-        $place->{street},
-        $place->{place},
-        $place->{url},
-        $place->{name},
+#        $place->{name},
+#        $place->{street},
+#        $place->{place},
+#        $place->{url},
+#        $place->{name},
         next_amsterdam_meeting(),
-        $place->{name},
-        $place->{street},
-        $place->{place},
-        $place->{url},
-        $place->{name},
+#        $place->{name},
+#        $place->{street},
+#        $place->{place},
+#        $place->{url},
+#        $place->{name},
     );
     my $body = sprintf(<<'    EOM', @args);
 [English version follows the dutch text]
 
-Amsterdam.pm staat voor de "Amsterdamse Perl Mongers", een groep van
-gebruikers van Perl. In tegenstelling tot wat de naam suggereert is
-Amsterdam.pm niet beperkt tot alleen Amsterdam, maar functioneert ook
-als de algemene Nederlandse gebruikersgroep. Iedereen die gelegenheid
-heeft is welkom, ook al bent u al bij Alphen.pm, Nijmegen.pm,
-Rotterdam.pm, Echt.pm, Exloo.pm, Groningen.pm of Wageningen.pm op
-bezoek geweest.
+LET OP: ivm. de coronacrisis, wordt ook de meeting van Amsterdam.pm van dinsdag %s afgelast.
 
-Amsterdam.pm organiseert informele bijeenkomsten waar Perl gebruikers
-kunnen samenkomen en informatie en gebruikservaringen met betrekking
-tot Perl kunnen uitwisselen.
-
-De bijeenkomsten vinden normaliter plaats op elke eerste dinsdag van
-de maand. Zoals gebruikelijk voor dit soort bijeenkomsten is er ook
-hier gelegenheid tot CAcert certificering en het uitwisselen van PGP
-keys. De voertaal binnen Amsterdam.pm is in principe Nederlands, maar
-indien nodig zal Engels worden gebruikt, b.v. om te communiceren met
-niet-Nederlandssprekende aanwezigen.
-
-De eerstvolgende bijeenkomst vindt plaats op dinsdag %s van
-20:00 tot 22:30 uur bij %s, %s, %s.
-Zie %s voor details.
-
-Liefhebbers van een etentje vooraf kunnen reeds tussen 18:00 en 18:30
-naar %s komen om een hapje te eten.
-
-Bezoek onze Web site http://perl.nl/amsterdam voor meer details.
+Zie http://perl.nl/amsterdam voor meer details.
 
 [English version]
 
-Amsterdam.pm stands for the Amsterdam Perl Mongers. We're basically a
-Perl user group. Despite its name, it is not local to the Amsterdam
-environment, but it welcomes Perl mongers from all over the
-Netherlands.
+Because of the corona crisis, the Amsterdam.pm meeting planned for tuesday %s will not take place.
 
-Amsterdam.pm organises informal meetings where Perl users can meet,
-and exchange information and experiences with regard to using Perl.
-
-The meetings are normally held every first Tuesday of the month. As
-conventional for these kind of meetings, there is also an opportunity
-to get CAcert certified, and to exchange PGP keys. Although the
-preferred language for communication is Dutch, English will be spoken
-if necessary.
-
-Our next meeting is Tuesday, %s, from 20:00 till 22:30 at the
-%s, %s, %s.
-See %s for a description.
-
-If you want to join some of us for dinner, please gather between 18:00
-and 18:30 at %s.
 
 See http://perl.nl/amsterdam for more details.
 -- 
     EOM
+#
+#[English version follows the dutch text]
+#
+#Amsterdam.pm staat voor de "Amsterdamse Perl Mongers", een groep van
+#gebruikers van Perl. In tegenstelling tot wat de naam suggereert is
+#Amsterdam.pm niet beperkt tot alleen Amsterdam, maar functioneert ook
+#als de algemene Nederlandse gebruikersgroep. Iedereen die gelegenheid
+#heeft is welkom, ook al bent u al bij Alphen.pm, Nijmegen.pm,
+#Rotterdam.pm, Echt.pm, Exloo.pm, Groningen.pm of Wageningen.pm op
+#bezoek geweest.
+#
+#Amsterdam.pm organiseert informele bijeenkomsten waar Perl gebruikers
+#kunnen samenkomen en informatie en gebruikservaringen met betrekking
+#tot Perl kunnen uitwisselen.
+#
+#De bijeenkomsten vinden normaliter plaats op elke eerste dinsdag van
+#de maand. Zoals gebruikelijk voor dit soort bijeenkomsten is er ook
+#hier gelegenheid tot CAcert certificering en het uitwisselen van PGP
+#keys. De voertaal binnen Amsterdam.pm is in principe Nederlands, maar
+#indien nodig zal Engels worden gebruikt, b.v. om te communiceren met
+#niet-Nederlandssprekende aanwezigen.
+#
+#De eerstvolgende bijeenkomst vindt plaats op dinsdag %s van
+#20:00 tot 22:30 uur bij %s, %s, %s.
+#Zie %s voor details.
+#
+#Liefhebbers van een etentje vooraf kunnen reeds tussen 18:00 en 18:30
+#naar %s komen om een hapje te eten.
+#
+#Bezoek onze Web site http://perl.nl/amsterdam voor meer details.
+#
+#[English version]
+#
+#Amsterdam.pm stands for the Amsterdam Perl Mongers. We're basically a
+#Perl user group. Despite its name, it is not local to the Amsterdam
+#environment, but it welcomes Perl mongers from all over the
+#Netherlands.
+#
+#Amsterdam.pm organises informal meetings where Perl users can meet,
+#and exchange information and experiences with regard to using Perl.
+#
+#The meetings are normally held every first Tuesday of the month. As
+#conventional for these kind of meetings, there is also an opportunity
+#to get CAcert certified, and to exchange PGP keys. Although the
+#preferred language for communication is Dutch, English will be spoken
+#if necessary.
+#
+#Our next meeting is Tuesday, %s, from 20:00 till 22:30 at the
+#%s, %s, %s.
+#See %s for a description.
+#
+#If you want to join some of us for dinner, please gather between 18:00
+#and 18:30 at %s.
+#
+#See http://perl.nl/amsterdam for more details.
+#-- 
+#    EOM
 
     my %address = (
         To => 'abe.timmerman@xs4all.nl',
